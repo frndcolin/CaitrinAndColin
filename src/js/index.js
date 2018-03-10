2400658def17732f525df4d4c7aa0e2b02162778
-l <-l>
2018-03-09 20:12:36 -0500

    added buttons to modal

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,21 @@ function initMap() {
     //-------------------------------------------------------------------
     var hotel1620MapInfo = document.querySelector('#hotel-1620');
     var pineHillsMapInfo = document.querySelector('#pine-hills');
-    var siteMap = document.querySelector('#map');
     var mapClose = document.querySelectorAll(".map-info-close");
-    
-    
-    
-    
-    
+    var siteMap = document.querySelector('#map');
+
+
+
+
+
     //-------------------------------------------------------------------
     //              CUSTOM MAP COLORS AND OPTIONS
     //-------------------------------------------------------------------
     var mapOptions = {
-        center: {lat: 41.9192124, lng: -70.6893377},
+        center: {
+            lat: 41.9192124,
+            lng: -70.6893377
+        },
         fullscreenControl: false,
         gestureHandling: 'cooperative',
         mapTypeControl: false,
@@ -284,10 +287,11 @@ function initMap() {
             }
         ]
     };
-    
-    
-    
-    
+
+
+
+
+
     //-------------------------------------------------------------------
     //              INIT MAP AND MARKERS
     //-------------------------------------------------------------------
@@ -295,38 +299,51 @@ function initMap() {
 
     var pineHillsMarker = new google.maps.Marker({
         icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520130197/wedding-site/pinehillsmarker.png',
-        position: {lat: 41.8813911, lng: -70.5988808},
+        position: {
+            lat: 41.8813911,
+            lng: -70.5988808
+        },
         map: map,
         title: 'Pine Hills',
         animation: google.maps.Animation.DROP
     });
     var hotel1620Marker = new google.maps.Marker({
         icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520094237/wedding-site/hotel1620marker.png',
-        position: {lat: 41.9619713, lng: -70.6703921},
+        position: {
+            lat: 41.9619713,
+            lng: -70.6703921
+        },
         map: map,
         title: 'Hotel 1620',
         animation: google.maps.Animation.DROP
     });
 
 
+
+
+
     //-------------------------------------------------------------------
     //                   ADD LISTENERS
     //-------------------------------------------------------------------
-    pineHillsMarker.addListener('click', () => pineHillsMapInfo.classList.replace('map-hide', 'map-show'));
-    hotel1620Marker.addListener('click', () => hotel1620MapInfo.classList.replace('map-hide', 'map-show'));
-    
+    pineHillsMarker.addListener('click', function () {
+        pineHillsMapInfo.classList.replace('map-hide', 'map-show')
+    });
+    hotel1620Marker.addListener('click', function () {
+        hotel1620MapInfo.classList.replace('map-hide', 'map-show')
+    });
+
     mapClose.forEach(function (mapBtn) {
         mapBtn.addEventListener('click', function () {
-            this.parentNode.classList.replace('map-show', 'map-hide');
+            mapBtn.parentNode.classList.replace('map-show', 'map-hide');
         });
     });
 }
 
 
 (function () {
-    //----------------------------------------------------------
-    //              Init global vendor libs
-    //----------------------------------------------------------
+    //-------------------------------------------------------------------------------------///
+    //                      INIT VENDOR LIBS                                               ///
+    //-------------------------------------------------------------------------------------///
     if (AOS) {
         AOS.init({
             disable: window.innerWidth < 720,
@@ -336,13 +353,22 @@ function initMap() {
         });
     }
 
+
+
+
+
+    //-------------------------------------------------------------------------------------///
+    //                      EMAIL FORM HANDLER                                             ///
+    //-------------------------------------------------------------------------------------///    
     var emailForm = document.querySelector('form#emailForm');
     var frmBtn = document.querySelector('button.frm-btn');
     var formMsg = document.querySelector('.formMsg');
+
+    // CAPTURE FORM SUBMISSION AND UPDATE DB
     frmBtn.addEventListener('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
-        let email = document.querySelector('input#email');
+        var email = document.querySelector('input#email');
         axios.post('/addEmail', {
                 email: email.value
             })
@@ -357,28 +383,30 @@ function initMap() {
             .catch(function (err) {
                 alert(err);
             });
-
     });
 
-    //================================================================
-    //          COUNTDOWN FUNCTIONS
-    //==================================================================
-    var clock = new Worker('./js/time.js');
 
-    var Display = function (arr) {
-        var self = this;
-        self.selections = arr;
 
-        arr.forEach(choice => {
-            self[choice] = document.querySelector('.date--' + choice);
-        });
-        return self;
-    }
+
+
+
+    //-------------------------------------------------------------------------------------///
+    //                      COUNTDOWN HANDLER                                              ///
+    //-------------------------------------------------------------------------------------/// 
+    var clock = new Worker('./js/time.js'),
+        Display = function (arr) {
+            var self = this;
+            self.selections = arr;
+
+            arr.forEach(choice => {
+                self[choice] = document.querySelector('.date--' + choice);
+            });
+            return self;
+        };
 
     Display.prototype.showTime = function (data) {
-        var self = this;
-        var time = data;
-
+        var self = this,
+            time = data;
         self.selections.forEach(title => {
             var date = self[title];
             if (date.innerHTML != time[title]) {
@@ -387,72 +415,116 @@ function initMap() {
         });
     }
 
+    // INIT COUNTDOWN
     var countdown = new Display(['months', 'days', 'hours', 'minutes', 'seconds']);
-
+    //HANDLE MESSAGES FROM WEB WORKER
     clock.onmessage = function (resp) {
         countdown.showTime(resp.data);
-        //    console.log(resp.data);
     };
 
 
 
-    //----------------------------------------------------------
-    //              capture elements
-    //----------------------------------------------------------
-
-    const buttons = document.querySelectorAll('.btn');
 
 
+    //-------------------------------------------------------------------------------------///
+    //                      BUTTON ANIMATIONS                                              ///
+    //-------------------------------------------------------------------------------------/// 
+    var buttons = document.querySelectorAll('.btn');
 
-
-
-    //----------------------------------------------------------
-    //              add event listeners
-    //----------------------------------------------------------
-    buttons.forEach(btn => {
-        btn.addEventListener('mouseover', () => {
-            this.classList.toggle('pulse');
+    buttons.forEach(function (btn) {
+        btn.addEventListener('mouseenter', function () {
+            btn.classList.toggle('pulse');
         });
-        btn.addEventListener('mouseleave', () => {
-            this.classList.toggle('pulse');
+        btn.addEventListener('mouseleave', function () {
+            btn.classList.toggle('pulse')
         });
     });
 
 
-    //===================================================================
-    var galleryImg = document.querySelectorAll('.gallery-item img');
-    var modal = document.querySelector('.img-box');
-    var modalImg = document.querySelector('.img-box-content img');
-    var modalClose = document.querySelector('.img-box-close');
 
-    galleryImg.forEach(function (img) {
-        img.addEventListener('click', function (e) {
-            e.stopPropagation();
-            var source = this.getAttribute('src');
-            modalImg.setAttribute('src', source);
-            modal.classList.toggle('zoomIn');
-            modal.classList.remove('zoomOut');
-        });
+
+
+    //-------------------------------------------------------------------------------------///
+    //                       MODAL HANDLERS                                                ///
+    //-------------------------------------------------------------------------------------/// 
+    var gallery = document.querySelector('.gallery'),
+        modal = document.querySelector('.img-modal'),
+        modalIMG = modal.querySelector('.modal-content img'),
+        imgLeft = modal.querySelector('.control-left'),
+        imgRight = modal.querySelector('.control-right'),
+        close = modal.querySelector('.modal-close'),
+        imgArr = [];
+
+    function changeImg(val) {
+        var current = imgArr.indexOf(modalIMG.getAttribute('src'));
+        if (current === 0 && val != 1) {
+            modalIMG.setAttribute('src', imgArr[imgArr.length - 1]);
+        } else {
+            modalIMG.setAttribute('src', imgArr[(current + val) % (imgArr.length)]);
+        }
+    }
+
+    gallery.addEventListener('click', function (e) {
+        var targetIMG = e.target;
+        if (targetIMG.tagName === 'IMG') {
+            var selected = imgArr.indexOf(targetIMG.getAttribute('src'));
+
+            modalIMG.setAttribute('src', imgArr[selected]);
+            modal.classList.add('opened');
+        }
     });
+
+    imgLeft.addEventListener('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        changeImg(-1);
+    });
+
+    imgRight.addEventListener('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        changeImg(1);
+    });
+
     modal.addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        modal.classList.replace('zoomIn', 'zoomOut');
-    });
-    modalClose.addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        modal.classList.replace('zoomIn', 'zoomOut');
+        var clicked = e.target;
+        if (clicked.tagName != 'IMG') {
+            modal.classList.remove('opened');
+        }
+        return false;
     });
 
+    close.addEventListener('click', function (e) {
+        e.stopPropagation();
+        modal.classList.remove('opened');
+    });
 
-    var navbar = document.querySelector('.menu');
-    var navLinks = document.querySelectorAll('.main-nav .nav-item');
-    var navBtn = document.querySelector('.nav-btn');
+    window.onload = function () {
+        var images = gallery.querySelectorAll('.gallery-item img');
+        var i = 0;
+        var max = images.length;
+        for (i; i < max; i++) {
+            if (images[i].getAttribute('data-src')) {
+                imgArr.push(images[i].getAttribute('data-src'));
+                images[i].setAttribute('src', images[i].getAttribute('data-src'));
+            }
+        }
+    };
+
+
+
+
+
+    //-------------------------------------------------------------------------------------///
+    //                       NAVIGATION HANDLERS                                           ///
+    //-------------------------------------------------------------------------------------/// 
+    var navbar = document.querySelector('.menu'),
+        navLinks = navbar.querySelectorAll('.main-nav .nav-item'),
+        navBtn = document.querySelector('.nav-btn');
 
     navBtn.addEventListener('click', function () {
         navbar.classList.toggle('open');
-        this.classList.toggle('pressed');
+        navBtn.classList.toggle('pressed');
     });
 
     navLinks.forEach(function (link) {
@@ -464,53 +536,51 @@ function initMap() {
         });
     });
 
-    
-    
-    var playlist = document.querySelector('.playlist');
-    var playbtn = document.querySelector('.playlist-btn');
-    var playlistClose = document.querySelector('.playlist-close');
-    var banner = document.querySelector('header');
-    var beenViewed = false;
-    
-    playbtn.addEventListener('click', function () {
-        playlist.classList.toggle('opened');
-    });
-    playbtn.addEventListener('mouseenter', function () {
-        this.classList.toggle('swing');
-    });
-    playbtn.addEventListener('mouseleave', function () {
-        this.classList.toggle('swing');
-    });
-    playlistClose.addEventListener('click', function () {
-        playlist.classList.toggle('opened');
-    });
-    
+
+
+
+
+    //-------------------------------------------------------------------------------------///
+    //                      PLAYLIST WINDOW HANDLERS                                       ///
+    //-------------------------------------------------------------------------------------/// 
+    var playlist = document.querySelector('.playlist'),
+        playbtn = playlist.querySelector('.playlist-btn'),
+        playlistClose = playlist.querySelector('.playlist-close'),
+        banner = document.querySelector('header'),
+        beenViewed = false;
+
     function closeOpenedWindow() {
         if (playlist.classList.contains('opened')) {
             playlist.classList.toggle('opened');
         }
     }
+
     function isInView(elementToCheck) {
         var bnrHt = banner.clientHeight;
         var target = elementToCheck.getBoundingClientRect().top + bnrHt;
         var screenHt = screen.height;
-        
+
         if (target > screenHt) {
             return false;
-        }
-        else if (target < screenHt - (screenHt - 50)){
+        } else if (target < screenHt - (screenHt - 50)) {
             return true;
         }
     }
+
+    playbtn.addEventListener('click', function () { playlist.classList.toggle('opened');});
+    playbtn.addEventListener('mouseenter', function () { playbtn.classList.toggle('swing');});
+    playbtn.addEventListener('mouseleave', function () { playbtn.classList.toggle('swing');});
+    playlistClose.addEventListener('click', function () { playlist.classList.toggle('opened');});
+
     window.onscroll = function () {
         if (isInView(banner) && !beenViewed) {
-                playlist.classList.add('opened');
-                setTimeout(function () {
-                    playlist.classList.remove('opened');
-                    beenViewed = true;
-                }, 2500);
+            playlist.classList.add('opened');
+            setTimeout(function () {
+                playlist.classList.remove('opened');
+                beenViewed = true;
+            }, 2500);
         } else {
-            closeOpenedWindow();   
+            closeOpenedWindow();
         }
     };
 }());
