2be102d95d31ecc73467f8d4da9fca4bf9d46435
frndcolin <frnd.colin@gmail.com>
2018-11-22 15:14:52 -0500

    spelling error

--- server.js
+++ server.js
@@ -1,44 +1,39 @@
-const express    = require('express');
-const bodyParser = require('body-parser');
-const favicon    = require('express-favicon');
-const compression = require('compression');
-const helmet     = require('helmet');
-const path       = require('path');
-const pug        = require('pug');
-const app        = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const favicon = require('express-favicon')
+const compression = require('compression')
+const helmet = require('helmet')
+const path = require('path')
+const pug = require('pug')
+const app = express()
 
-
-
-const port = process.env.PORT || 3000;
-const index = require('./routes/index');
-const playlist = require('./routes/playlist');
+const port = process.env.PORT || 3000
+const index = require('./routes/index')
+const playlist = require('./routes/playlist')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: '7 days' }))
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: '7 days'}));
-
-
-app.use('/', index);
-app.use('/playlist', playlist);
+app.use('/', index)
+app.use('/playlist', playlist)
 
 // error handlers
-
 // catch 404 and forward to error handler
-//app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 //    var err = new Error('Not Found');
 //    err.status = 404;
 //    res.render(404);
 //    next(err);
-//});
+// });
 
-app.listen(port, function() {
-    console.log(`Server is running on ${port}...`);
-});
+app.listen(port, function () {
+  console.log(`Server is running on ${port}...`)
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,324 +1,315 @@
-function initMap() {
-    //-------------------------------------------------------------------
-    //                      MAP ELEMENTS
-    //-------------------------------------------------------------------
-    var hotel1620MapInfo = document.querySelector('#hotel-1620');
-    var pineHillsMapInfo = document.querySelector('#pine-hills');
-    var mapClose = document.querySelectorAll(".map-info-close");
-    var siteMap = document.querySelector('#map');
+function initMap () {
+  // -------------------------------------------------------------------
+  //                      MAP ELEMENTS
+  // -------------------------------------------------------------------
+  var hotel1620MapInfo = document.querySelector('#hotel-1620');
+  var pineHillsMapInfo = document.querySelector('#pine-hills');
+  var mapClose = document.querySelectorAll('.map-info-close');
+  var siteMap = document.querySelector('#map');
 
-
-
-
-
-    //-------------------------------------------------------------------
-    //              CUSTOM MAP COLORS AND OPTIONS
-    //-------------------------------------------------------------------
-    var mapOptions = {
-        center: {
-            lat: 41.9192124,
-            lng: -70.6893377
-        },
-        fullscreenControl: false,
-        gestureHandling: 'cooperative',
-        mapTypeControl: false,
-        streetViewControl: false,
-        disableDefaultUI: true,
-        zoomControl: false,
-        zoom: 10,
-        styles: [
-            {
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#ebe3cd"
+  //-------------------------------------------------------------------
+  //              CUSTOM MAP COLORS AND OPTIONS
+  //-------------------------------------------------------------------
+  var mapOptions = {
+    center: {
+      lat: 41.9192124,
+      lng: -70.6893377
+    },
+    fullscreenControl: false,
+    gestureHandling: 'cooperative',
+    mapTypeControl: false,
+    streetViewControl: false,
+    disableDefaultUI: true,
+    zoomControl: false,
+    zoom: 10,
+    styles: [{
+      'elementType': 'geometry',
+        'stylers': [
+                 {
+                        'color': '#ebe3cd'
                     }
                 ]
             },
             {
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#523735"
+                        'color': '#523735'
                     }
                 ]
             },
             {
-                "elementType": "labels.text.stroke",
-                "stylers": [
+                'elementType': 'labels.text.stroke',
+                'stylers': [
                     {
-                        "color": "#f5f1e6"
+                        'color': '#f5f1e6'
                     }
                 ]
             },
             {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [
+                'featureType': 'administrative',
+                'elementType': 'geometry.stroke',
+                'stylers': [
                     {
-                        "color": "#c9b2a6"
+                        'color': '#c9b2a6'
                     }
                 ]
             },
             {
-                "featureType": "administrative.land_parcel",
-                "elementType": "geometry.stroke",
-                "stylers": [
+                'featureType': 'administrative.land_parcel',
+                'elementType': 'geometry.stroke',
+                'stylers': [
                     {
-                        "color": "#dcd2be"
+                        'color': '#dcd2be'
                     }
                 ]
             },
             {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels",
-                "stylers": [
+                'featureType': 'administrative.land_parcel',
+                'elementType': 'labels',
+                'stylers': [
                     {
-                        "visibility": "off"
+                        'visibility': 'off'
                     }
                 ]
             },
             {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'administrative.land_parcel',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#ae9e90"
+                        'color': '#ae9e90'
                     }
                 ]
             },
             {
-                "featureType": "landscape.natural",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'landscape.natural',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#dfd2ae"
+                        'color': '#dfd2ae'
                     }
                 ]
             },
             {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'poi',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#dfd2ae"
+                        'color': '#dfd2ae'
                     }
                 ]
             },
             {
-                "featureType": "poi",
-                "elementType": "labels.text",
-                "stylers": [
+                'featureType': 'poi',
+                'elementType': 'labels.text',
+                'stylers': [
                     {
-                        "visibility": "off"
+                        'visibility': 'off'
                     }
                 ]
             },
             {
-                "featureType": "poi",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'poi',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#93817c"
+                        'color': '#93817c'
                     }
                 ]
             },
             {
-                "featureType": "poi.business",
-                "stylers": [
+                'featureType': 'poi.business',
+                'stylers': [
                     {
-                        "visibility": "off"
+                        'visibility': 'off'
                     }
                 ]
             },
             {
-                "featureType": "poi.park",
-                "elementType": "geometry.fill",
-                "stylers": [
+                'featureType': 'poi.park',
+                'elementType': 'geometry.fill',
+                'stylers': [
                     {
-                        "color": "#a5b076"
+                        'color': '#a5b076'
                     }
                 ]
             },
             {
-                "featureType": "poi.park",
-                "elementType": "labels.text",
-                "stylers": [
+                'featureType': 'poi.park',
+                'elementType': 'labels.text',
+                'stylers': [
                     {
-                        "visibility": "off"
+                        'visibility': 'off'
                     }
                 ]
             },
             {
-                "featureType": "poi.park",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'poi.park',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#447530"
+                        'color': '#447530'
                     }
                 ]
             },
             {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'road',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#f5f1e6"
+                        'color': '#f5f1e6'
                     }
                 ]
             },
             {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'road.arterial',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#fdfcf8"
+                        'color': '#fdfcf8'
                     }
                 ]
             },
             {
-                "featureType": "road.highway",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'road.highway',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#f8c967"
+                        'color': '#f8c967'
                     }
                 ]
             },
             {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [
+                'featureType': 'road.highway',
+                'elementType': 'geometry.stroke',
+                'stylers': [
                     {
-                        "color": "#e9bc62"
+                        'color': '#e9bc62'
                     }
                 ]
             },
             {
-                "featureType": "road.highway.controlled_access",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'road.highway.controlled_access',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#e98d58"
+                        'color': '#e98d58'
                     }
                 ]
             },
             {
-                "featureType": "road.highway.controlled_access",
-                "elementType": "geometry.stroke",
-                "stylers": [
+                'featureType': 'road.highway.controlled_access',
+                'elementType': 'geometry.stroke',
+                'stylers': [
                     {
-                        "color": "#db8555"
+                        'color': '#db8555'
                     }
                 ]
             },
             {
-                "featureType": "road.local",
-                "elementType": "labels",
-                "stylers": [
+                'featureType': 'road.local',
+                'elementType': 'labels',
+                'stylers': [
                     {
-                        "visibility": "off"
+                        'visibility': 'off'
                     }
                 ]
             },
             {
-                "featureType": "road.local",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'road.local',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#806b63"
+                        'color': '#806b63'
                     }
                 ]
             },
             {
-                "featureType": "transit.line",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'transit.line',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#dfd2ae"
+                        'color': '#dfd2ae'
                     }
                 ]
             },
             {
-                "featureType": "transit.line",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'transit.line',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#8f7d77"
+                        'color': '#8f7d77'
                     }
                 ]
             },
             {
-                "featureType": "transit.line",
-                "elementType": "labels.text.stroke",
-                "stylers": [
+                'featureType': 'transit.line',
+                'elementType': 'labels.text.stroke',
+                'stylers': [
                     {
-                        "color": "#ebe3cd"
+                        'color': '#ebe3cd'
                     }
                 ]
             },
             {
-                "featureType": "transit.station",
-                "elementType": "geometry",
-                "stylers": [
+                'featureType': 'transit.station',
+                'elementType': 'geometry',
+                'stylers': [
                     {
-                        "color": "#dfd2ae"
+                        'color': '#dfd2ae'
                     }
                 ]
             },
             {
-                "featureType": "water",
-                "elementType": "geometry.fill",
-                "stylers": [
+                'featureType': 'water',
+                'elementType': 'geometry.fill',
+                'stylers': [
                     {
-                        "color": "#b9d3c2"
+                        'color': '#b9d3c2'
                     },
                     {
-                        "saturation": -20
+                        'saturation': -20
                     }
                 ]
             },
             {
-                "featureType": "water",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                'featureType': 'water',
+                'elementType': 'labels.text.fill',
+                'stylers': [
                     {
-                        "color": "#92998d"
+                        'color': '#92998d'
                     }
                 ]
             }
         ]
     };
 
+  //-------------------------------------------------------------------
+  //              INIT MAP AND MARKERS
+  //-------------------------------------------------------------------
+  var map = new google.maps.Map(siteMap, mapOptions);
 
-
-
-
-    //-------------------------------------------------------------------
-    //              INIT MAP AND MARKERS
-    //-------------------------------------------------------------------
-    var map = new google.maps.Map(siteMap, mapOptions);
-
-    var pineHillsMarker = new google.maps.Marker({
-        icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520130197/wedding-site/pinehillsmarker.png',
-        position: {
-            lat: 41.8813911,
-            lng: -70.5988808
-        },
-        map: map,
-        title: 'Pine Hills',
-        animation: google.maps.Animation.DROP
-    });
-    var hotel1620Marker = new google.maps.Marker({
-        icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520094237/wedding-site/hotel1620marker.png',
-        position: {
-            lat: 41.9619713,
-            lng: -70.6703921
-        },
-        map: map,
-        title: 'Hotel 1620',
-        animation: google.maps.Animation.DROP
-    });
+  var pineHillsMarker = new google.maps.Marker({
+    icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520130197/wedding-site/pinehillsmarker.png',
+    position: {
+      lat: 41.8813911,
+      lng: -70.5988808
+    },
+    map: map,
+    title: 'Pine Hills',
+    animation: google.maps.Animation.DROP
+  });
+  var hotel1620Marker = new google.maps.Marker({
+    icon: 'http://res.cloudinary.com/frndcolin/image/upload/f_auto,q_40/v1520094237/wedding-site/hotel1620marker.png',
+    position: {
+      lat: 41.9619713,
+      lng: -70.6703921
+    },
+      map: map,
+      title: 'Hotel 1620',
+      animation: google.maps.Animation.DROP
+  });
 
 
 
@@ -371,10 +362,10 @@ function initMap() {
         
         axios.post('/addEmail', { email: email.value })
             .then(function (resp) {
-                if (resp.status === 200 && resp.data === "success") {
+                if (resp.status === 200 && resp.data === 'success') {
                     emailForm.style.display = 'none';
                     formMsg.style.display = 'block';
-                    formMsg.innerHTML = "Thanks! We'll keep you up to date!";
+                    formMsg.innerHTML = 'Thanks! We'll keep you up to date!';
                 }
             })
             .catch(function (err) {
